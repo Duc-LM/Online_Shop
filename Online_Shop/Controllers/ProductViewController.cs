efccc42f158aec231fd784b5f9763e351bd2047e
htg30599 <1701040146@s.hanu.edu.vn>
2021-10-15 21:05:00 +0700

    Productstest

---
+++ Online_Shop/Controllers/ProductViewController.cs
@@ -0,0 +1,24 @@
+﻿using Online_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Shop.Controllers
+{
+    public class ProductViewController : BaseController
+    {
+        // GET: ProductView
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dell()
+        {
+            Product product = new Product();
+            return View();
+        }
+    }
+}
