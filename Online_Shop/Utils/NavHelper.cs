5e245bbe35d21bba0a5b537e53666b57678d5760
htg30599 <1701040146@s.hanu.edu.vn>
2021-10-17 12:27:22 +0700

    no message

---
+++ Online_Shop/Utils/NavHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Online_Shop.Utils
+{
+    public class NavHelper
+    {
+        public static List<Category> GetCategories()
+        {
+            return new ShopEntities().Categories.ToList();
+        }
+        public static List<string> GetManufacturers()
+        {
+            return new ShopEntities().Specs.Select(m => m.Manufacturer).ToList();
+        }
+    }
+}
